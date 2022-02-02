bf2079ac5c3398b01089ed189d2a23e459152a4c
박중섭 <>
2022-02-03 02:25:03 +0900

    💄 TS-TODO 마크업 & UI설계 완료

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { FC, useState } from "react";
+import "./App.css";
 
-function App() {
+const App: FC = () => {
+  const [task, setTask] = useState<string>("");
+  const [deadline, setDeadline] = useState<number>(0);
+  const [todoList, setTodoList] = useState<string>("");
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="header">
+        <div className="inputContainer">
+          <input type="text" placeholder="Task..." />
+          <input type="number" placeholder="Deadline (in Days)..." />
+        </div>
+        <button>Add Task</button>
+      </div>
+      <div className="todoList"></div>
     </div>
   );
-}
+};
 
 export default App;
